843a3d58b0d49d4b7e32a6d8a1418e78e0c7faa3
Guna-meda <meda.gunavathi25@gmail.com>
2024-06-20 17:18:53 +0530

    new commit

--- paint.js
+++ paint.js
@@ -1,2 +1,11 @@
-console.log (`a1`);
+slideOpen ();
+
+function slideOpen () {
+let btn = document.querySelector('#btn');
+let slidebar = document.querySelector('.slidebar');
+
+btn.onclick = function() {
+    slidebar.classList.toggle('active');
+}
+}
 
